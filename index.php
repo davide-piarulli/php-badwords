071454fdd097e7d3d3bab364d97efbba74ef3568
davide-piarulli <davide.piarulli.1@gmail.com>
2024-04-12 16:15:16 +0100

    [add] pagina form

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
+<?php
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Badwords</title>
 </head>
+
 <body>
-  
+  <div class="container">
+    <h1>Ciao PHP</h1>
+    <a href="form-post.html">Clicca qui per compilare il Form.</a>
+  </div>
 </body>
+
 </html>
---
+++ pagina-atterraggio-form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ricezione dati</title>
+</head>
+
+<body>
+  <div class="container my-5 ">
+    <h1>Dati Ricevuti</h1>
+    <div>
+      <h4>Il paragrafo ricevuto è lungo: ed è il seguente:</h4>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+    </div>
+
+    <div>
+      <h4>La parola censurata è:</h4>
+      <p></p>
+    </div>
+  </div>
+</body>
+
+</html>
