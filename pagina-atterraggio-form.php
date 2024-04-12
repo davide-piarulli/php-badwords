45c6c18764d26e12bebafda099fe97d76022112e
davide-piarulli <davide.piarulli.1@gmail.com>
2024-04-12 16:27:02 +0100

    [add] paragrafo

--- pagina-atterraggio-form.php
+++ pagina-atterraggio-form.php
@@ -1,3 +1,9 @@
+<?php
+$testo = $_POST['paragrafo'];
+$lunghezza_paragrafo = strlen($testo);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,8 +17,8 @@
   <div class="container my-5 ">
     <h1>Dati Ricevuti</h1>
     <div>
-      <h4>Il paragrafo ricevuto è lungo: ed è il seguente:</h4>
-      <textarea name="" id="" cols="30" rows="10"></textarea>
+      <h4>Il paragrafo ricevuto è lungo: <?php echo $lunghezza_paragrafo ?> ed è il seguente: </h4>
+      <textarea name="paragrafo" id="" cols="30" rows="10"><?php echo $testo ?></textarea>
     </div>
 
     <div>
